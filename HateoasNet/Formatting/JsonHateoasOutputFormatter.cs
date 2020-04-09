025f727350bc5843e507c88d71a05a350309b620
icarotorres <icaro.stuart@gmail.com>
2020-04-09 15:14:08 -0300

    refactor(Formatter): refactor logic in smaller files with their specific responsibilities.

---
+++ HateoasNet/Formatting/JsonHateoasOutputFormatter.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters;
+
+namespace HateoasNet.Formatting
+{
+	public class JsonHateoasOutputFormatter : OutputFormatter
+	{
+		public JsonHateoasOutputFormatter()
+		{
+			SupportedMediaTypes.Add("application/json");
+			SupportedMediaTypes.Add("application/json+hateoas");
+		}
+
+		public override Task WriteResponseBodyAsync(OutputFormatterWriteContext context)
+		{
+			if (context.Object is SerializableError error)
+			{
+				var errorOutput = JsonSerializer.Serialize(error);
+				context.HttpContext.Response.ContentType = SupportedMediaTypes.First();
+				return context.HttpContext.Response.WriteAsync(errorOutput);
+			}
+
+			var resourceConverter = new ResourceConverter(context.HttpContext);
+			var hateoasWriter = new HateoasWriter(context, resourceConverter);
+			var hateoasStringOutput = hateoasWriter.WriteHateoasOutput();
+
+			context.HttpContext.Response.ContentType = SupportedMediaTypes.Last();
+			return context.HttpContext.Response.WriteAsync(hateoasStringOutput);
+		}
+	}
+}
