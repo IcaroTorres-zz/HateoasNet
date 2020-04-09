5a53bc355ae269c76297c932f5eecab5f4c2ad0b
icarotorres <icaro.stuart@gmail.com>
2020-04-09 15:21:36 -0300

    refactor(extension): refactor code exposing an extension to enable package features under the HateoasNet.DependencyInjection namespace.

---
+++ HateoasNet/DependencyInjection/HateoasExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using HateoasNet.Core;
+using HateoasNet.Formatting;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HateoasNet.DependencyInjection
+{
+	public static class HateoasExtensions
+	{
+		public static IMvcBuilder EnableHateoas(this IMvcBuilder builder, Action<HateoasNetOptions> options = null)
+		{
+			if (options != null) builder.Services.Configure(options);
+			builder.Services.AddSingleton<IActionContextAccessor, ActionContextAccessor>();
+			builder.AddMvcOptions(o => o.OutputFormatters.Add(new JsonHateoasOutputFormatter()));
+			return builder;
+		}
+	}
+}
