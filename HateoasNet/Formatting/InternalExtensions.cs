13814dae096d4440e8b1225fdfe1e370193623ac
icarotorres <icaro.stuart@gmail.com>
2020-04-09 15:18:05 -0300

    feat(compatibility): move link creating logic to internal an extension. Set compatibility to new net core 3.1 version and netstandard 2.0.

--- HateoasNet/Formatting/InternalExtensions.cs
+++ HateoasNet/Formatting/InternalExtensions.cs
@@ -1,30 +1,41 @@
-﻿using System;
+﻿#if NETCOREAPP3_1
+using Microsoft.AspNetCore.Mvc.ActionConstraints;
+#elif NETSTANDARD2_0
+using Microsoft.AspNetCore.Mvc.Internal;
+#endif
+using System;
+using System.Collections.Generic;
 using System.Linq;
+using HateoasNet.Core;
 using HateoasNet.Resources;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.Infrastructure;
 using Microsoft.AspNetCore.Mvc.Routing;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
-#if NETCOREAPP3_1
-using Microsoft.AspNetCore.Mvc.ActionConstraints;
 
-#elif NETSTANDARD2_0
-using Microsoft.AspNetCore.Mvc.Internal;
-#endif
-
-namespace HateoasNet
+namespace HateoasNet.Formatting
 {
-	internal static class JsonHateoasFormatterExtensions
+	internal static class InternalExtensions
 	{
 		private static T GetService<T>(this HttpContext context)
 		{
 			return (T) context.RequestServices.GetRequiredService(typeof(T));
 		}
 
-		internal static T GetPropertyAsValue<T>(this object source, string propertyName)
+		private static T GetPropertyAsValue<T>(this object source, string propertyName)
 		{
 			return (T) source.GetType().GetProperty(propertyName)?.GetValue(source);
+		}
+
+		internal static Pagination<object> ExtractPagination(this object source)
+		{
+			var items = source.GetPropertyAsValue<IEnumerable<object>>(nameof(Pagination<object>.Data));
+			var count = source.GetPropertyAsValue<long>(nameof(Pagination<object>.Count));
+			var pageSize = source.GetPropertyAsValue<int>(nameof(Pagination<object>.PageSize));
+			var page = source.GetPropertyAsValue<int>(nameof(Pagination<object>.Page));
+
+			return new Pagination<object>(items, count, pageSize, page);
 		}
 
 		internal static Resource ApplyHateoasLinks(this Resource resource, Type sourceType, HttpContext context)
