d627ad2ac0ee9a01fc91c6862c2201c9250d46ad
icarotorres <icaro.stuart@gmail.com>
2020-04-09 15:15:47 -0300

    refactor(resources): add class for data to hateoas resource conversion.

---
+++ HateoasNet/Formatting/ResourceConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using HateoasNet.Resources;
+using Microsoft.AspNetCore.Http;
+
+namespace HateoasNet.Formatting
+{
+	public class ResourceConverter
+	{
+		private readonly HttpContext _httpContext;
+
+		public ResourceConverter(HttpContext httpContext)
+		{
+			_httpContext = httpContext;
+		}
+
+		public PaginationResource<Resource> ToPagination(Pagination<Resource> source, Type type)
+		{
+			return (PaginationResource<Resource>) new PaginationResource<Resource>(source)
+				.ApplyHateoasLinks(type, _httpContext);
+		}
+
+		public EnumerableResource<Resource> ToEnumerable(IEnumerable<Resource> source, Type type)
+		{
+			return (EnumerableResource<Resource>) new EnumerableResource<Resource>(source)
+				.ApplyHateoasLinks(type, _httpContext);
+		}
+
+		public SingleResource ToSingle(object source, Type type)
+		{
+			return (SingleResource) new SingleResource(source).ApplyHateoasLinks(type, _httpContext);
+		}
+	}
+}
