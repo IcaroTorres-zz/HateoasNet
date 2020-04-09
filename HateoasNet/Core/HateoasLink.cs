aede6c3b36970087def28e241278e841b28e9bdd
icarotorres <icaro.stuart@gmail.com>
2020-04-09 15:20:26 -0300

    feat(links): add descriptive methods to pass functions for link creation while enabling resources per type. refactor(namespace): namespace separation.

---
+++ HateoasNet/Core/HateoasLink.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.AspNetCore.Routing;
+
+namespace HateoasNet.Core
+{
+	public sealed class HateoasLink<T> : IHateoasLink<T> where T : class
+	{
+		private Func<T, bool> PredicateFunction;
+		private Func<T, RouteValueDictionary> ValuesFunction;
+
+		internal HateoasLink(string routeName, Func<T, RouteValueDictionary> valuesFunction, Func<T, bool> predicate)
+		{
+			RouteName = routeName;
+			ValuesFunction = valuesFunction;
+			PredicateFunction = predicate;
+			SourceType = typeof(T);
+		}
+
+		public Type SourceType { get; }
+		public string RouteName { get; }
+
+		public RouteValueDictionary GetRouteDictionary(object routeData) => ValuesFunction(routeData as T);
+
+		public bool CheckLinkPredicate(object routeData) => PredicateFunction(routeData as T);
+
+		public IHateoasLink<T> WithRouteData(Func<T, object> routeDataFunction)
+		{
+			if (routeDataFunction == null) throw new ArgumentNullException(nameof(routeDataFunction));
+
+			ValuesFunction = new Func<T, RouteValueDictionary>(
+				sourceValue => new RouteValueDictionary(
+					(routeDataFunction ?? (e => null))(sourceValue)
+				)
+			);
+			return this;
+		}
+
+		public IHateoasLink<T> DisplayedWhen(Func<T, bool> predicate)
+		{
+			PredicateFunction = predicate ?? throw new ArgumentNullException(nameof(predicate));
+			return this;
+		}
+	}
+}
--- HateoasNet/Core/IHateoasLink.cs
+++ HateoasNet/Core/IHateoasLink.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using Microsoft.AspNetCore.Routing;
 
-namespace HateoasNet
+namespace HateoasNet.Core
 {
 	public interface IHateoasLink
 	{
@@ -9,5 +9,11 @@ namespace HateoasNet
 		string RouteName { get; }
 		RouteValueDictionary GetRouteDictionary(object routeData);
 		bool CheckLinkPredicate(object routeData);
+	}
+	
+	public interface IHateoasLink<out T> : IHateoasLink where T : class
+	{
+		IHateoasLink<T> WithRouteData(Func<T, object> routeDataFunction);
+		IHateoasLink<T> DisplayedWhen(Func<T, bool> predicate);
 	}
 }
