b71afbc8dd808c5caede77f8adedfbb68b8fa5f7
icarotorres <icaro.stuart@gmail.com>
2020-04-11 19:10:04 -0300

    chore(startup): update Startup.cs with calls to new expansion methods for each type of hateoas setup.

--- Sample/Startup.cs
+++ Sample/Startup.cs
@@ -18,7 +18,15 @@ namespace Sample
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
-			services.AddControllers().BootstrapHateoasFormatter();
+			/*
+			 * switch this lines to test different configuration styles
+			 * Check 'HateoasSetupExtensions.cs' file for details of each one
+			 */
+			
+			//services.AddControllers().HateoasSampleSimpleLinks();
+			services.AddControllers().HateoasSampleWithManualMapping();
+			//services.AddControllers().HateoasSampleWithSeparatedFiles();
+			// services.AddControllers().HateoasSampleFromAssembly();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
