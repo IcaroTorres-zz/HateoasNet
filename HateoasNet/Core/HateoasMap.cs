7f36a3a84440a7c03cd11a5122a02fda9d994bef
icarotorres <icaro.stuart@gmail.com>
2020-04-11 18:58:18 -0300

    feat(hateoas-map): add HateoasMap class for mapping resource type with links grouped allowing more readability to configuration setup.

---
+++ HateoasNet/Core/HateoasMap.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using HateoasNet.Core.Abstractions;
+using Microsoft.AspNetCore.Routing;
+
+namespace HateoasNet.Core
+{
+	public sealed class HateoasMap<T> : IHateoasMap where T : class
+	{
+		private readonly List<IHateoasLink> _hateoasLinks = new List<IHateoasLink>();
+		public IReadOnlyList<IHateoasLink> GetLinks() => _hateoasLinks.AsReadOnly();
+
+		public IHateoasLink<T> HasLink(string routeName)
+		{
+			var routeDataFunction = new Func<T, object>(e => null);
+			var predicate = new Func<T, bool>(e => true);
+			return HasLink(routeName, routeDataFunction, predicate);
+		}
+
+		public IHateoasLink<T> HasLink(string routeName, Func<T, object> routeDataFunction, Func<T, bool> predicate = null)
+		{
+			if (routeDataFunction == null) throw new ArgumentNullException(nameof(routeDataFunction));
+
+			var valuesFunction = new Func<T, RouteValueDictionary>(
+				e => new RouteValueDictionary(routeDataFunction(e)));
+
+			predicate ??= t => true;
+
+			var hateoasLink = new HateoasLink<T>(routeName, valuesFunction, predicate);
+			_hateoasLinks.Add(hateoasLink);
+			return hateoasLink;
+		}
+	}
+}
