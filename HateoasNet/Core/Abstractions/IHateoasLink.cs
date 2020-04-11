779f8ed668e5838c5719c7b922401b2953956180
icarotorres <icaro.stuart@gmail.com>
2020-04-11 18:54:08 -0300

    refactor(hateoas-links): move into HateoasNet.Core.Abstractions and simplify method names.

--- HateoasNet/Core/Abstractions/IHateoasLink.cs
+++ HateoasNet/Core/Abstractions/IHateoasLink.cs
@@ -1,19 +1,19 @@
 ﻿using System;
 using Microsoft.AspNetCore.Routing;
 
-namespace HateoasNet.Core
+namespace HateoasNet.Core.Abstractions
 {
 	public interface IHateoasLink
 	{
 		Type SourceType { get; }
 		string RouteName { get; }
 		RouteValueDictionary GetRouteDictionary(object routeData);
-		bool CheckLinkPredicate(object routeData);
+		bool CheckPredicateForData(object routeData);
 	}
 	
 	public interface IHateoasLink<out T> : IHateoasLink where T : class
 	{
-		IHateoasLink<T> WithRouteData(Func<T, object> routeDataFunction);
-		IHateoasLink<T> DisplayedWhen(Func<T, bool> predicate);
+		IHateoasLink<T> WithData(Func<T, object> routeDataFunction);
+		IHateoasLink<T> When(Func<T, bool> predicate);
 	}
 }
