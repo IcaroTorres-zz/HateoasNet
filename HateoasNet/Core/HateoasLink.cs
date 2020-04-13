42b37641c9246223d459c3f105937167a257ceff
icarotorres <icaro.stuart@gmail.com>
2020-04-13 14:28:49 -0300

    refactor(hateoas-links): rename methods and remove resourceType property.

--- HateoasNet/Core/HateoasLink.cs
+++ HateoasNet/Core/HateoasLink.cs
@@ -1,40 +1,38 @@
 ﻿using System;
-using HateoasNet.Core.Abstractions;
+using HateoasNet.Abstractions;
 using Microsoft.AspNetCore.Routing;
 
 namespace HateoasNet.Core
 {
 	public sealed class HateoasLink<T> : IHateoasLink<T> where T : class
 	{
-		private Func<T, bool> PredicateFunction;
-		private Func<T, RouteValueDictionary> ValuesFunction;
+		private Func<T, bool> _predicateFunction;
+		private Func<T, RouteValueDictionary> _valuesFunction;
 
 		internal HateoasLink(string routeName, Func<T, RouteValueDictionary> valuesFunction, Func<T, bool> predicate)
 		{
 			RouteName = routeName;
-			ValuesFunction = valuesFunction;
-			PredicateFunction = predicate;
-			SourceType = typeof(T);
+			_valuesFunction = valuesFunction;
+			_predicateFunction = predicate;
 		}
 
-		public Type SourceType { get; }
 		public string RouteName { get; }
 
-		public RouteValueDictionary GetRouteDictionary(object routeData) => ValuesFunction(routeData as T);
+		public RouteValueDictionary GetRouteDictionary(object routeData) => _valuesFunction(routeData as T);
 
-		public bool CheckPredicateForData(object routeData) => PredicateFunction(routeData as T);
+		public bool IsDisplayable(object routeData) => _predicateFunction(routeData as T);
 
-		public IHateoasLink<T> WithData(Func<T, object> routeDataFunction)
+		public IHateoasLink<T> HasRouteData(Func<T, object> routeDataFunction)
 		{
 			if (routeDataFunction == null) throw new ArgumentNullException(nameof(routeDataFunction));
 
-			ValuesFunction = (T source) => new RouteValueDictionary(routeDataFunction(source));
+			_valuesFunction = (T source) => new RouteValueDictionary(routeDataFunction(source));
 			return this;
 		}
 
-		public IHateoasLink<T> When(Func<T, bool> predicate)
+		public IHateoasLink<T> HasConditional(Func<T, bool> predicate)
 		{
-			PredicateFunction = predicate ?? throw new ArgumentNullException(nameof(predicate));
+			_predicateFunction = predicate ?? throw new ArgumentNullException(nameof(predicate));
 			return this;
 		}
 	}
