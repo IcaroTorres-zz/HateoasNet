6705a6d87f215f109209c82bc0602ed98bc84c3e
icarotorres <icaro.stuart@gmail.com>
2020-04-13 14:32:58 -0300

    refactor(hateoas-serializer): add class responsible only for hateoas resource serialization.

---
+++ HateoasNet/Formatting/HateoasSerializer.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json;
+using HateoasNet.Abstractions;
+using HateoasNet.Converters;
+using HateoasNet.Resources;
+
+namespace HateoasNet.Formatting
+{
+	public class HateoasSerializer : IHateoasSerializer
+	{
+		public string SerializeResource(Resource resource)
+		{
+			var serializerOptions = new JsonSerializerOptions
+			{
+				IgnoreNullValues = true,
+				PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+			};
+			serializerOptions.Converters.Add(new GuidConverter());
+			serializerOptions.Converters.Add(new DateTimeConverter());
+			return JsonSerializer.Serialize(resource, serializerOptions);
+		}
+	}
+}
