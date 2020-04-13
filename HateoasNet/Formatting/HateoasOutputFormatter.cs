6b3e0ab16a11066490dc784b1ea32cba7923a260
icarotorres <icaro.stuart@gmail.com>
2020-04-13 14:37:01 -0300

    refactor(hateoas-output-formatter): refactor to get HateoasWriter from dependency injection services.

--- HateoasNet/Formatting/HateoasOutputFormatter.cs
+++ HateoasNet/Formatting/HateoasOutputFormatter.cs
@@ -1,9 +1,11 @@
 ﻿using System.Linq;
 using System.Text.Json;
 using System.Threading.Tasks;
+using HateoasNet.Abstractions;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace HateoasNet.Formatting
 {
@@ -24,12 +26,11 @@ namespace HateoasNet.Formatting
 				return context.HttpContext.Response.WriteAsync(errorOutput);
 			}
 
-			var resourceConverter = new ResourceConverter(context.HttpContext);
-			var hateoasWriter = new HateoasWriter(context, resourceConverter);
-			var hateoasStringOutput = hateoasWriter.WriteHateoasOutput();
+			var hateoasWriter = context.HttpContext.RequestServices.GetRequiredService<IHateoasWriter>();
+			var formattedResponse = hateoasWriter.Write(context.Object, context.ObjectType);
 
 			context.HttpContext.Response.ContentType = SupportedMediaTypes.Last();
-			return context.HttpContext.Response.WriteAsync(hateoasStringOutput);
+			return context.HttpContext.Response.WriteAsync(formattedResponse);
 		}
 	}
 }
