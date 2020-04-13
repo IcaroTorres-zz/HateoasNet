83a41da9123a756e342d347f900f2ef6ee2f7b31
icarotorres <icaro.stuart@gmail.com>
2020-04-13 14:44:19 -0300

    chore(dependency-injection): add extension method registering required services and dependencies of HateoasOutputFormatter.

--- HateoasNet/DependencyInjection/HateoasExtensions.cs
+++ HateoasNet/DependencyInjection/HateoasExtensions.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using HateoasNet.Abstractions;
 using HateoasNet.Core;
 using HateoasNet.Formatting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.Infrastructure;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -8,12 +10,19 @@ namespace HateoasNet.DependencyInjection
 {
 	public static class HateoasExtensions
 	{
+		public static IServiceCollection AddHateoasServices(this IServiceCollection services)
+		{
+			return services.AddSingleton<IActionContextAccessor, ActionContextAccessor>()
+				.AddSingleton<IHttpContextAccessor, HttpContextAccessor>()
+				.AddTransient<IHateoasConverter, HateoasConverter>()
+				.AddTransient<IHateoasSerializer, HateoasSerializer>()
+				.AddTransient<IHateoasWriter, HateoasWriter>();
+		}
+
 		public static IMvcBuilder ConfigureHateoasMap(this IMvcBuilder builder, Action<HateoasConfiguration> config = null)
 		{
 			if (config != null) builder.Services.Configure(config);
-			builder.Services.AddSingleton<IActionContextAccessor, ActionContextAccessor>();
-			builder.AddMvcOptions(o => o.OutputFormatters.Add(new HateoasOutputFormatter()));
-			return builder;
+			return builder.AddMvcOptions(o => o.OutputFormatters.Add(new HateoasOutputFormatter()));
 		}
 	}
 }
