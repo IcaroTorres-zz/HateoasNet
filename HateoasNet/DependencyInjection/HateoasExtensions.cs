cb5a7d99f1b7959088bfb23c6ea14c234f18b4d3
icarotorres <icaro.stuart@gmail.com>
2020-04-14 14:11:34 -0300

    chore(setup): update setup registering configuration interface service and not allowing null for configuration action.

--- HateoasNet/DependencyInjection/HateoasExtensions.cs
+++ HateoasNet/DependencyInjection/HateoasExtensions.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using HateoasNet.Abstractions;
-using HateoasNet.Core;
 using HateoasNet.Formatting;
+using HateoasNet.Mapping;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.Infrastructure;
 using Microsoft.Extensions.DependencyInjection;
@@ -14,14 +14,18 @@ namespace HateoasNet.DependencyInjection
 		{
 			return services.AddSingleton<IActionContextAccessor, ActionContextAccessor>()
 				.AddSingleton<IHttpContextAccessor, HttpContextAccessor>()
+				.AddTransient<IHateoasConfiguration, HateoasConfiguration>()
 				.AddTransient<IHateoasConverter, HateoasConverter>()
 				.AddTransient<IHateoasSerializer, HateoasSerializer>()
 				.AddTransient<IHateoasWriter, HateoasWriter>();
 		}
 
-		public static IMvcBuilder ConfigureHateoasMap(this IMvcBuilder builder, Action<HateoasConfiguration> config = null)
+		public static IMvcBuilder ConfigureHateoasMap(this IMvcBuilder builder,
+			Action<IHateoasConfiguration> hateoasConfiguration)
 		{
-			if (config != null) builder.Services.Configure(config);
+			if (hateoasConfiguration == null) throw new ArgumentNullException(nameof(hateoasConfiguration));
+
+			builder.Services.Configure(hateoasConfiguration);
 			return builder.AddMvcOptions(o => o.OutputFormatters.Add(new HateoasOutputFormatter()));
 		}
 	}
