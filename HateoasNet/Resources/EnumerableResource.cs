a7b2be619e0f4ede1d40e21361140fae103d0ec4
icarotorres <icaro.stuart@gmail.com>
2020-04-14 13:59:16 -0300

    fix(enumerable-resource): fix type parameter constraints.

--- HateoasNet/Resources/EnumerableResource.cs
+++ HateoasNet/Resources/EnumerableResource.cs
@@ -3,12 +3,13 @@ using System.Linq;
 
 namespace HateoasNet.Resources
 {
-	public class EnumerableResource<T> : Resource where T : class
+	public class EnumerableResource<T> : Resource where T : Resource
 	{
 		public EnumerableResource(IEnumerable<T> data) : base(data)
 		{
 			DataList = data.ToList();
 		}
+
 		private List<T> DataList { get; }
 		public override object Data => DataList;
 	}
