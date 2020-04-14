cb8ff1d5f2cde8a6ddfe57409344733c3c6ed210
icarotorres <icaro.stuart@gmail.com>
2020-04-14 12:59:30 -0300

    refactor: add interface contract for dependency inject configuration.

---
+++ HateoasNet/Abstractions/IHateoasConfiguration.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using HateoasNet.Core;
+
+namespace HateoasNet.Abstractions
+{
+	public interface IHateoasConfiguration
+	{
+		IEnumerable<IHateoasLink> GetMappedLinks(Type sourceType, object resourceData);
+		IHateoasConfiguration Map<T>(Action<HateoasMap<T>> mapper) where T : class;
+		IHateoasConfiguration ApplyConfiguration<T>(IHateoasBuilder<T> builder) where T : class;
+		IHateoasConfiguration ApplyConfigurationsFromAssembly(Assembly assembly);
+	}
+}
