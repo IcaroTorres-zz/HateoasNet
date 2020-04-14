282522d2c73fb1ec269a0dce25f078b7a18b52bd
icarotorres <icaro.stuart@gmail.com>
2020-04-13 21:22:35 -0300

    refactor(hateoas-link): turn RouteDictionaryFunction and predicateFunction into public get.

--- HateoasNet/Core/HateoasLink.cs
+++ HateoasNet/Core/HateoasLink.cs
@@ -6,33 +6,34 @@ namespace HateoasNet.Core
 {
 	public sealed class HateoasLink<T> : IHateoasLink<T> where T : class
 	{
-		private Func<T, bool> _predicateFunction;
-		private Func<T, RouteValueDictionary> _valuesFunction;
-
-		internal HateoasLink(string routeName, Func<T, RouteValueDictionary> valuesFunction, Func<T, bool> predicate)
+		internal HateoasLink(string routeName,
+			Func<T, RouteValueDictionary> routeDictionaryFunction,
+			Func<T, bool> predicateFunction)
 		{
 			RouteName = routeName;
-			_valuesFunction = valuesFunction;
-			_predicateFunction = predicate;
+			RouteDictionaryFunction = routeDictionaryFunction;
+			PredicateFunction = predicateFunction;
 		}
 
+		public Func<T, bool> PredicateFunction { get; private set; }
+		public Func<T, RouteValueDictionary> RouteDictionaryFunction { get; private set; }
 		public string RouteName { get; }
 
-		public RouteValueDictionary GetRouteDictionary(object routeData) => _valuesFunction(routeData as T);
+		public RouteValueDictionary GetRouteDictionary(object routeData) => RouteDictionaryFunction(routeData as T);
 
-		public bool IsDisplayable(object routeData) => _predicateFunction(routeData as T);
+		public bool IsDisplayable(object routeData) => PredicateFunction(routeData as T);
 
 		public IHateoasLink<T> HasRouteData(Func<T, object> routeDataFunction)
 		{
 			if (routeDataFunction == null) throw new ArgumentNullException(nameof(routeDataFunction));
 
-			_valuesFunction = (T source) => new RouteValueDictionary(routeDataFunction(source));
+			RouteDictionaryFunction = (T source) => new RouteValueDictionary(routeDataFunction(source));
 			return this;
 		}
 
 		public IHateoasLink<T> HasConditional(Func<T, bool> predicate)
 		{
-			_predicateFunction = predicate ?? throw new ArgumentNullException(nameof(predicate));
+			PredicateFunction = predicate ?? throw new ArgumentNullException(nameof(predicate));
 			return this;
 		}
 	}
