f770b326a31bc753b9cd742f8dc091ed0555c950
icarotorres <icaro.stuart@gmail.com>
2020-04-19 01:09:24 -0300

    refactor: add abstraction of factory for ResourceLink construction.

---
+++ HateoasNet/Abstractions/IResourceLinkFactory.cs
@@ -0,0 +1,9 @@
+﻿using HateoasNet.Resources;
+
+namespace HateoasNet.Abstractions
+{
+	public interface IResourceLinkFactory
+	{
+		ResourceLink Create(Resource resource, IHateoasLink hateoasLink);
+	}
+}
