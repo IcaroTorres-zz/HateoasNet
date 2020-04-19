365295f86d282c3d8fdacc23f21bef33eb872696
icarotorres <icaro.stuart@gmail.com>
2020-04-19 01:14:22 -0300

    refactor(pagination): replace constructor parameter by its Abstraction. remove references of serialization library.

--- HateoasNet/Resources/PaginationResource.cs
+++ HateoasNet/Resources/PaginationResource.cs
@@ -1,12 +1,12 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text.Json.Serialization;
+using HateoasNet.Abstractions;
 
 namespace HateoasNet.Resources
 {
 	public class PaginationResource<T> : Resource where T : Resource
 	{
-		public PaginationResource(Pagination<T> values) : base(values.Data)
+		public PaginationResource(IPagination<T> values) : base(values.Data)
 		{
 			DataList = values.Data.ToList();
 			Count = values.Count;
@@ -17,10 +17,10 @@ namespace HateoasNet.Resources
 
 		private List<T> DataList { get; }
 		public override object Data => DataList;
-		[JsonPropertyName("inPage")] public int InPage => DataList.Count;
-		[JsonPropertyName("page")] public int Page { get; }
-		[JsonPropertyName("pageSize")] public int PageSize { get; }
-		[JsonPropertyName("pages")] public int Pages { get; }
-		[JsonPropertyName("count")] public long Count { get; }
+		public int InPage => DataList.Count;
+		public int Page { get; }
+		public int PageSize { get; }
+		public int Pages { get; }
+		public long Count { get; }
 	}
 }
