fe34f92306c02374e2216c28485e5af123c95cdf
icarotorres <icaro.stuart@gmail.com>
2020-04-20 18:43:37 -0300

    feat: add implementations for method and url discovering.

---
+++ HateoasNet/Abstractions/IHttpMethodFinder.cs
@@ -0,0 +1,15 @@
+﻿namespace HateoasNet.Abstractions
+{
+	/// <summary>
+	/// Interface for specific platform implementations of HttpMethod discovering of routes by name.
+	/// </summary>
+	public interface IHttpMethodFinder
+	{
+		/// <summary>
+		/// Find the HttpMethod string value for a route with given name.
+		/// </summary>
+		/// <param name="routeName">Route name to find.</param>
+		/// <returns>The string value representing HttpMethod.</returns>
+		public string Find(string routeName);
+	}
+}
---
+++ HateoasNet/Abstractions/IUrlBuilder.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace HateoasNet.Abstractions
+{
+	/// <summary>
+	/// Custom interface for creating urls for routes
+	/// </summary>
+	public interface IUrlBuilder
+	{
+		/// <summary>
+		/// Build an url string for a route with given name and values from route data.
+		/// </summary>
+		/// <param name="routeName">Name of desired route to discover the url.</param>
+		/// <param name="routeData">Route data object to look for parameters and query strings.</param>
+		/// <returns>Generated Url string.</returns>
+		public string Build(string routeName, object routeData);
+
+		/// <summary>
+		/// Build an url string for a route with given name and values from route data.
+		/// </summary>
+		/// <param name="routeName">Name of desired route to discover the url.</param>
+		/// <param name="routeDictionary">Route dictionary to look for parameters and query strings.</param>
+		/// <returns>Generated Url string.</returns>
+		public string Build(string routeName, IDictionary<string, object> routeDictionary);
+	}
+}
