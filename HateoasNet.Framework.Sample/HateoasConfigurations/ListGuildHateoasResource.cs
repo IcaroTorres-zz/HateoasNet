0ab469d09d3e5b583d8142ed1f2ae8aab209b188
icarotorres <icaro.stuart@gmail.com>
2020-04-21 20:55:00 -0300

    refactor(framework-sample): move and rename configuration classes.

---
+++ HateoasNet.Framework.Sample/HateoasConfigurations/ListGuildHateoasResource.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using HateoasNet.Abstractions;
+using HateoasNet.Framework.Sample.Models;
+
+namespace HateoasNet.Framework.Sample.HateoasConfigurations
+{
+	public class ListGuildHateoasResource : IHateoasResourceConfiguration<List<Guild>>
+	{
+		public void Configure(IHateoasResource<List<Guild>> resource)
+		{
+			resource.HasLink("get-guilds");
+			resource.HasLink("create-guild");
+		}
+	}
+}
---
+++ HateoasNet.Framework.Sample/HateoasConfigurations/ListMemberHateoasResource.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using HateoasNet.Abstractions;
+using HateoasNet.Framework.Sample.Models;
+
+namespace HateoasNet.Framework.Sample.HateoasConfigurations
+{
+	public class ListMemberHateoasResource : IHateoasResourceConfiguration<List<Member>>
+	{
+		public void Configure(IHateoasResource<List<Member>> resource)
+		{
+			resource.HasLink("get-members");
+			resource.HasLink("invite-member");
+			resource.HasLink("create-member");
+		}
+	}
+}
