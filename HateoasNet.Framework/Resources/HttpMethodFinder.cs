c02809c40314b58efe483f7a9411364988e734be
icarotorres <icaro.stuart@gmail.com>
2020-04-21 20:39:59 -0300

    refactor(framework-library): add net core specific implementations for url and http method discovering.

---
+++ HateoasNet.Framework/Resources/HttpMethodFinder.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http.Controllers;
+using HateoasNet.Abstractions;
+
+namespace HateoasNet.Framework.Resources
+{
+	public class HttpMethodFinder : IHttpMethodFinder
+	{
+		private readonly IEnumerable<HttpActionDescriptor> _actionDescriptors;
+
+		public HttpMethodFinder(IEnumerable<HttpActionDescriptor> actionDescriptors)
+		{
+			_actionDescriptors = actionDescriptors;
+		}
+
+		public string Find(string routeName)
+		{
+			return _actionDescriptors
+				.SingleOrDefault(x => x.GetRouteAttribute().Name == routeName)
+				.GetMethod().Method;
+		}
+	}
+}
