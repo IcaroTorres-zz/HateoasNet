c001fbbe372052ad9d2ca3413bb4e15a8054fa64
icarotorres <icaro.stuart@gmail.com>
2020-04-21 17:48:38 -0300

    docs(abstractions): xml docs for IHateoasSerializer

--- HateoasNet/Abstractions/IHateoasSerializer.cs
+++ HateoasNet/Abstractions/IHateoasSerializer.cs
@@ -2,8 +2,16 @@
 
 namespace HateoasNet.Abstractions
 {
+	/// <summary>
+	/// Represents a custom Serializer for  <see cref="Resource"/> formatted with HATEOAS.
+	/// </summary>
 	public interface IHateoasSerializer
 	{
+		/// <summary>
+		/// Serialize a formatted <see cref="Resource"/> instance.
+		/// </summary>
+		/// <param name="resource">The formatted <see cref="Resource"/>.</param>
+		/// <returns>Json <see cref="string"/> representing formatted <see cref="Resource"/> output.</returns>
 		string SerializeResource(Resource resource);
 	}
 }
