b2a9017b558d36359b8975152fab39774849b49a
icarotorres <icaro.stuart@gmail.com>
2020-04-21 18:34:19 -0300

    docs(abstractions): xml docs for IPagination

--- HateoasNet/Abstractions/IPagination.cs
+++ HateoasNet/Abstractions/IPagination.cs
@@ -3,17 +3,32 @@ using System.Collections.Generic;
 
 namespace HateoasNet.Abstractions
 {
+	/// <summary>
+	/// Represents an <see cref="ICollection"/> paginated.
+	/// </summary>
 	public interface IPagination
 	{
 		long Count { get; }
 		int PageSize { get; }
 		int Page { get; }
 		int Pages { get; }
+		
+		/// <summary>
+		/// Gets the paginated <see cref="ICollection"/> items as <see cref="IEnumerable" />.
+		/// </summary>
+		/// <returns></returns>
 		IEnumerable GetEnumeration();
 	}
 
+	/// <summary>
+	/// Represents an <see cref="ICollection{T}"/> paginated.
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
 	public interface IPagination<out T> : IPagination
 	{
+		/// <summary>
+		/// The paginated <see cref="ICollection{T}"/> items as <see cref="IEnumerable{T}"/>.
+		/// </summary>
 		IEnumerable<T> Data { get; }
 	}
 }
