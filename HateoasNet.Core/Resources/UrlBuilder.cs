3d36b3c6f8b5b531d59cd7d0af28d32bc591e5ac
icarotorres <icaro.stuart@gmail.com>
2020-04-21 20:39:38 -0300

    refactor(core-library): add net core specific implementations for url and http method discovering.

---
+++ HateoasNet.Core/Resources/UrlBuilder.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using HateoasNet.Abstractions;
+using HateoasNet.Mapping;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.AspNetCore.Mvc.Routing;
+
+namespace HateoasNet.Core.Resources
+{
+	public class UrlBuilder : IUrlBuilder
+	{
+		private readonly IUrlHelper _urlHelper;
+
+		public UrlBuilder(IUrlHelperFactory urlHelperFactory, IActionContextAccessor actionContextAccessor)
+		{
+			_urlHelper = urlHelperFactory.GetUrlHelper(actionContextAccessor.ActionContext);
+		}
+
+		public string Build(string routeName, object routeData) => Build(routeName, routeData.ToRouteDictionary());
+
+		public string Build(string routeName, IDictionary<string, object> routeDictionary) =>
+			_urlHelper.Link(routeName, routeDictionary);
+	}
+}
