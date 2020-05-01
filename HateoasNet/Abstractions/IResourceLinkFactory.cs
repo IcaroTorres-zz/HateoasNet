32cd63e258c75aae2e42dd186da44964a0559a7c
icarotorres <icaro.stuart@gmail.com>
2020-04-30 23:32:29 -0300

    refactor(common-library): remove discarded abstractions gathering all url and method discovering into IResourceLinkFactory implementations.

--- HateoasNet/Abstractions/IResourceLinkFactory.cs
+++ HateoasNet/Abstractions/IResourceLinkFactory.cs
@@ -10,16 +10,16 @@ namespace HateoasNet.Abstractions
 	{
 		/// <summary>
 		///   Creates a HATEOAS <see cref="ResourceLink" /> using <paramref name="rel" />
-		///   with <paramref name="routeValueDictionary" /> to discover route Url and HTTP method.
+		///   with <paramref name="routeValues" /> to discover route Url and HTTP method.
 		/// </summary>
 		/// <param name="rel">
 		///   An endpoint Route Name assigned on an action method attribute.
 		///   <para>see also <seealso cref="IHateoasLink.RouteName" />.</para>
 		/// </param>
-		/// <param name="routeValueDictionary">
+		/// <param name="routeValues">
 		///   The <see cref="IDictionary{TKey,TValue}" /> of <see cref="string" />, <see cref="object" /> holding route values.
 		/// </param>
 		/// <returns>The generated <see cref="ResourceLink" /> instance for HATEOAS output.</returns>
-		ResourceLink Create(string rel, IDictionary<string, object> routeValueDictionary);
+		ResourceLink Create(string rel, IDictionary<string, object> routeValues);
 	}
 }
