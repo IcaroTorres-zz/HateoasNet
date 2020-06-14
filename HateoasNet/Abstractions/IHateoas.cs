26fa047bd6389fa509945c3cb58715d8a2ba8989
icarotorres <icaro.stuart@gmail.com>
2020-06-13 22:35:26 -0300

    refactor(abstractions): update abstractions with breaking changes to new version 2-0-0.

---
+++ HateoasNet/Abstractions/IHateoas.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace HateoasNet.Abstractions
+{
+    /// <summary>
+    ///   Represents a Factory for creating collections of <see cref="HateoasLink" /> output instances.
+    /// </summary>
+    public interface IHateoas
+    {
+        /// <summary>
+        ///   Gets available collection of <see cref="HateoasLink"/> using <see cref="IHateoasSource{T}" /> configuration
+        ///   of <typeparamref name="T" /> with <paramref name="source" /> as original value.
+        /// </summary>
+        /// <param name="source">Original value to look for mapped links.</param>
+        /// <typeparam name="T">type parameter of <see cref="IHateoasSource{T}" />  configuration.</typeparam>
+        /// <returns>A <see cref="IEnumerable{T}" /> of <see cref="HateoasLink"/> ietms.</returns>
+        IEnumerable<HateoasLink> Generate<T>(T source);
+    }
+}
---
+++ HateoasNet/Abstractions/IHateoasSource.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace HateoasNet.Abstractions
+{
+    /// <summary>
+    ///   Represents a HATEOAS Source to generate <see cref="IEnumerable{T}"/> of <see cref="HateoasLink"/> 
+    ///   items associate with <see cref="IHateoasLinkBuilder" />.
+    /// </summary>
+    public interface IHateoasSource
+    {
+        /// <summary>
+        ///   Gets all <see cref="IHateoasLinkBuilder" /> associated with this <see cref="IHateoasSource" /> instance.
+        /// </summary>
+        /// <returns><see cref="IEnumerable{IHateoasLink}" /> representing Its Links.</returns>
+        IEnumerable<IHateoasLinkBuilder> GetLinkBuilders();
+    }
+
+    /// <summary>
+    ///   Represents a HATEOAS resource <typeparamref name="T" /> to be formatted associate with <see cref="IHateoasLinkBuilder{T}" />
+    ///   configurations.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IHateoasSource<T> : IHateoasSource where T : class
+    {
+        /// <summary>
+        ///   Adds our replaces an <see cref="IHateoasLinkBuilder{T}" /> configuration with <paramref name="routeName" /> to this
+        ///   <see cref="IHateoasSource{T}" /> instance.
+        /// </summary>
+        /// <param name="routeName">
+        ///   An endpoint Route Name assigned on an action method attribute.
+        ///   <para>see also <seealso cref="IHateoasLinkBuilder.RouteName" />.</para>
+        /// </param>        
+        /// <returns>A <see cref="IHateoasLinkBuilder{T}" /> configuration generated with <paramref name="routeName" />.</returns>
+        IHateoasLinkBuilder<T> AddLink(string routeName);
+    }
+}
