86e99325e75048bffde487f1afbeae234d4df18e
icarotorres <icaro.stuart@gmail.com>
2020-06-13 22:47:40 -0300

    refactor(core-sample): update controllers for new demonstration with main abstration IHateoas.

--- HateoasNet.Core.Sample/Controllers/InvitesController.cs
+++ HateoasNet.Core.Sample/Controllers/InvitesController.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using HateoasNet.Abstractions;
 using HateoasNet.Core.Sample.JsonData;
 using HateoasNet.Core.Sample.Models;
-using HateoasNet.Resources;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HateoasNet.Core.Sample.Controllers
@@ -13,29 +13,35 @@ namespace HateoasNet.Core.Sample.Controllers
 	public class InvitesController : ControllerBase
 	{
 		private readonly List<Invite> _invites;
+		private readonly IHateoas _hateoas;
 
-		public InvitesController(Seeder seeder)
+		public InvitesController(Seeder seeder, IHateoas hateoas)
 		{
 			_invites = seeder.Seed<Invite>();
+			_hateoas = hateoas;
 		}
 
 		[HttpGet("{id:Guid}", Name = "get-invite")]
 		public IActionResult Get(Guid id)
 		{
 			var invite = _invites.SingleOrDefault(i => i.Id == id);
-			return invite != null ? Ok(invite) : NotFound() as IActionResult;
+			var links = _hateoas.Generate(invite);
+			return invite != null ? Ok(new { data = invite, links }) : NotFound() as IActionResult;
 		}
 
 		[HttpGet(Name = "get-invites")]
 		public IActionResult Get([FromQuery(Name = "pageSize")] int pageSize = 5)
 		{
-			return Ok(new Pagination<Invite>(_invites.Take(pageSize), _invites.Count, pageSize));
+			var invites = _invites.Take(pageSize);
+			var links = _hateoas.Generate(invites);
+			return Ok(new { data = invites, links });
 		}
 
 		[HttpPost(Name = "invite-member")]
 		public IActionResult Post([FromBody] Invite invite)
 		{
-			return CreatedAtRoute("get-invite", new {id = invite.Id}, invite);
+			var links = _hateoas.Generate(invite);
+			return CreatedAtRoute("get-invite", new {id = invite.Id}, new { data = invite, links });
 		}
 
 		[HttpPatch("{id}/accept", Name = "accept-invite")]
@@ -44,7 +50,8 @@ namespace HateoasNet.Core.Sample.Controllers
 			var invite = _invites.SingleOrDefault(i => i.Id == id);
 			if (invite == null) return NotFound();
 			invite.Status = InviteStatuses.Accepted;
-			return Ok(invite);
+			var links = _hateoas.Generate(invite);
+			return Ok(new { data = invite, links });
 		}
 
 		[HttpPatch("{id}/decline", Name = "decline-invite")]
@@ -53,7 +60,8 @@ namespace HateoasNet.Core.Sample.Controllers
 			var invite = _invites.SingleOrDefault(i => i.Id == id);
 			if (invite == null) return NotFound();
 			invite.Status = InviteStatuses.Declined;
-			return Ok(invite);
+			var links = _hateoas.Generate(invite);
+			return Ok(new { data = invite, links });
 		}
 
 		[HttpPatch("{id}/cancel", Name = "cancel-invite")]
@@ -62,7 +70,8 @@ namespace HateoasNet.Core.Sample.Controllers
 			var invite = _invites.SingleOrDefault(i => i.Id == id);
 			if (invite == null) return NotFound();
 			invite.Status = InviteStatuses.Canceled;
-			return Ok(invite);
+			var links = _hateoas.Generate(invite);
+			return Ok(new { data = invite, links });
 		}
 	}
 }
--- HateoasNet.Core.Sample/Controllers/MembersController.cs
+++ HateoasNet.Core.Sample/Controllers/MembersController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using HateoasNet.Abstractions;
 using HateoasNet.Core.Sample.JsonData;
 using HateoasNet.Core.Sample.Models;
 using Microsoft.AspNetCore.Mvc;
@@ -12,36 +13,42 @@ namespace HateoasNet.Core.Sample.Controllers
 	public class MembersController : ControllerBase
 	{
 		private readonly List<Member> _members;
+		private readonly IHateoas hateoas;
 
-		public MembersController(Seeder seeder)
+		public MembersController(Seeder seeder, IHateoas hateoas)
 		{
 			_members = seeder.Seed<Member>();
+			this.hateoas = hateoas;
 		}
 
 		[HttpGet("{id:Guid}", Name = "get-member")]
 		public IActionResult Get(Guid id)
 		{
 			var member = _members.SingleOrDefault(i => i.Id == id);
-			return member != null ? Ok(member) : NotFound() as IActionResult;
+			var links = hateoas.Generate(member);
+			return member != null ? Ok(new { data = member, links }) : NotFound() as IActionResult;
 		}
 
 		[HttpGet(Name = "get-members")]
 		public IActionResult GetAll([FromQuery] Guid guildId)
 		{
 			var members = _members.Where(m => m.GuildId == guildId || guildId == Guid.Empty).ToList();
-			return Ok(members);
+			var links = hateoas.Generate(members);
+			return Ok(new { data = members, links });
 		}
 
 		[HttpPost(Name = "create-member")]
 		public IActionResult Create([FromBody] Member member)
 		{
-			return CreatedAtRoute("get-member", new {id = member.Id}, member);
+			var links = hateoas.Generate(member);
+			return CreatedAtRoute("get-member", new {id = member.Id}, new { data = member, links });
 		}
 
 		[HttpPut("{id:Guid}", Name = "update-member")]
 		public IActionResult Update([FromBody] Member member)
 		{
-			return Ok(member);
+			var links = hateoas.Generate(member);
+			return Ok(new { data = member, links });
 		}
 
 		[HttpPatch("{id}/promote", Name = "promote-member")]
@@ -50,7 +57,8 @@ namespace HateoasNet.Core.Sample.Controllers
 			var member = _members.SingleOrDefault(i => i.Id == id);
 			if (member == null) return NotFound();
 			member.IsGuildMaster = true;
-			return Ok(member);
+			var links = hateoas.Generate(member);
+			return Ok(new { data = member, links });
 		}
 
 		[HttpPatch("{id}/demote", Name = "demote-member")]
@@ -59,7 +67,8 @@ namespace HateoasNet.Core.Sample.Controllers
 			var member = _members.SingleOrDefault(i => i.Id == id);
 			if (member == null) return NotFound();
 			member.IsGuildMaster = false;
-			return Ok(member);
+			var links = hateoas.Generate(member);
+			return Ok(new { data = member, links });
 		}
 
 		[HttpPatch("{id}/leave", Name = "leave-guild")]
@@ -69,7 +78,8 @@ namespace HateoasNet.Core.Sample.Controllers
 			if (member == null) return NotFound();
 			member.Guild = null;
 			member.GuildId = null;
-			return Ok(member);
+			var links = hateoas.Generate(member);
+			return Ok(new { data = member, links });
 		}
 	}
 }
