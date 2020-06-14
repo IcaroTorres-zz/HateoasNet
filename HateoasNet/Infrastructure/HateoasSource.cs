c65b415b56e51f324171e59c5ab570aafecd40a9
icarotorres <icaro.stuart@gmail.com>
2020-06-13 22:44:33 -0300

    refactor(structure): simplify fundamental items in root namespace. discard unused implementations.

---
+++ HateoasNet/Infrastructure/HateoasSource.cs
@@ -0,0 +1,28 @@
+﻿using HateoasNet.Abstractions;
+using System;
+using System.Collections.Generic;
+
+namespace HateoasNet.Infrastructure
+{
+    /// <inheritdoc cref="IHateoasSource" />
+    public sealed class HateoasSource<T> : IHateoasSource<T> where T : class
+	{
+		private readonly Dictionary<string, IHateoasLinkBuilder> _linkBuilders = new Dictionary<string, IHateoasLinkBuilder>();
+
+		internal HateoasSource()
+		{
+		}
+
+		public IEnumerable<IHateoasLinkBuilder> GetLinkBuilders()
+		{
+			return _linkBuilders.Values;
+		}
+
+		public IHateoasLinkBuilder<T> AddLink(string routeName)
+		{
+			if (string.IsNullOrWhiteSpace(routeName)) throw new ArgumentNullException(nameof(routeName));
+
+			return (_linkBuilders[routeName] = new HateoasLinkBuilder<T>(routeName)) as IHateoasLinkBuilder<T>;
+		}
+	}
+}
