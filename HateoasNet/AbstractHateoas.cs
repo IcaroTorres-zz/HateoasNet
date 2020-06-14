fc5f86eaf51280ec11d226fa400e6c54fd388de6
icarotorres <icaro.stuart@gmail.com>
2020-06-14 14:55:03 -0300

    feat(custom-hateoas): add abstractions enable feature for customizing hateoas output.

---
+++ HateoasNet/AbstractHateoas.cs
@@ -0,0 +1,23 @@
+﻿using HateoasNet.Abstractions;
+using System.Collections.Generic;
+
+namespace HateoasNet
+{
+	/// <inheritdoc cref="IHateoas{TOutput}" />
+	public abstract class AbstractHateoas<TOutput> : IHateoas<TOutput>
+	{
+		private readonly IHateoas _hateoas;
+
+		public AbstractHateoas(IHateoas hateoas)
+		{
+			_hateoas = hateoas;
+		}
+
+		public TOutput Generate<TSource>(TSource source)
+		{
+			return GenerateCustom(_hateoas.Generate(source));
+		}
+
+		public abstract TOutput GenerateCustom(IEnumerable<HateoasLink> links);
+	}
+}
