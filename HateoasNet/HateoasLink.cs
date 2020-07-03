451ea9cc00e4e53c4d94a81384931101c7df30c4
icarotorres <icaro.stuart@gmail.com>
2020-07-03 00:14:27 -0300

    refact(sonar)? remove minor smells.

--- HateoasNet/HateoasLink.cs
+++ HateoasNet/HateoasLink.cs
@@ -20,18 +20,18 @@ namespace HateoasNet
         ///   The Url to access some available action of the subject source containing this <see cref="HateoasLink" />.
         /// </summary>
         /// <value>Url <see langword="string" /> value.</value>
-        public string Href { get; } = string.Empty;
+        public string Href { get; }
 
         /// <summary>
         ///   The name assigned to the action available in the subject source containing this <see cref="HateoasLink" />.
         /// </summary>
         /// <value>The <see langword="string" /> value of assigned <see cref="Rel" />.</value>
-        public string Rel { get; } = string.Empty;
+        public string Rel { get; }
 
         /// <summary>
         ///   The HTTP method used to access the action available in the subject source containing this <see cref="HateoasLink" />.
         /// </summary>
         /// <value>The <see langword="string" /> value of HTTP method.</value>
-        public string Method { get; } = string.Empty;
+        public string Method { get; }
     }
 }
--- HateoasNet/HateoasSampleBuilder.cs
+++ HateoasNet/HateoasSampleBuilder.cs
@@ -7,7 +7,7 @@ namespace HateoasNet.Tests.TestHelpers
     {
         public void Build(IHateoasSource<HateoasSample> source)
         {
-            source.AddLink("test1").When(x => x.Id != null && x.Id != Guid.Empty).HasRouteData(x => new { id = x.Id });
+            source.AddLink("test1").When(x => x.Id != Guid.Empty).HasRouteData(x => new { id = x.Id });
             source.AddLink("test2").When(x => !string.IsNullOrWhiteSpace(x.ZipCode)).HasRouteData(x => new { zipcode = x.ZipCode });
         }
     }
