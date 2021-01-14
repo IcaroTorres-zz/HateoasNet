93858ceb17f9a6ae8c72f2eb99eb8591f170a1ca
icarotorres <icaro.stuart@gmail.com>
2021-01-14 18:54:27 -0300

    refactor(hateoas): let ihateoas to generate links without type parameter.

--- HateoasNet/IHateoas.cs
+++ HateoasNet/IHateoas.cs
@@ -9,12 +9,11 @@ namespace HateoasNet
     {
         /// <summary>
         ///   Gets available collection of <see cref="HateoasLink"/> using <see cref="IHateoasSource{T}" /> configuration
-        ///   of <typeparamref name="T" /> with <paramref name="source" /> as original value.
+        ///   for the <see cref="Type"/> of <paramref name="source" /> given as original value.
         /// </summary>
         /// <param name="source">Original value to look for mapped links.</param>
-        /// <typeparam name="T">type parameter of <see cref="IHateoasSource{T}" />  configuration.</typeparam>
-        /// <returns>A <see cref="IEnumerable{T}" /> of <see cref="HateoasLink"/> ietms.</returns>
-        IEnumerable<HateoasLink> Generate<T>(T source);
+        /// <returns>A <see cref="IEnumerable{T}" /> of <see cref="HateoasLink"/> items.</returns>
+        IEnumerable<HateoasLink> Generate(object source);
     }
 
     /// <summary>
@@ -24,11 +23,10 @@ namespace HateoasNet
     {
         /// <summary>
         ///   Gets <typeparamref name="TOutput"/> value transformed from available collection of <see cref="HateoasLink"/> using <see cref="IHateoasSource{T}" />
-        ///   configuration of <typeparamref name="TSource" /> with <paramref name="source" /> as original value.
+        ///   configuration for the <see cref="Type"/> of <paramref name="source" /> given as original value.
         /// </summary>
         /// <param name="source">Original value to look for mapped links.</param>
-        /// <typeparam name="TSource">type parameter of <see cref="IHateoasSource{T}" />  configuration.</typeparam>
         /// <returns>A custom <see cref="TOutput"/> created from <see cref="IEnumerable{T}" /> of <see cref="HateoasLink"/> ietms.</returns>
-        TOutput Generate<TSource>(TSource source);
+        TOutput Generate(object source);
     }
 }
